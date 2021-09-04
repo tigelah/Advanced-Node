e49e283b1261a748422fc7ae2149d87ed6f8b182
tigelah <lokoportitimao2009@hotmail.com>
2021-09-04 15:28:22 -0300

    move LoadFacebookAutheticationUseCase to its own file

--- tests/data/use-case/facebook-authentication.spec.ts
+++ tests/data/use-case/facebook-authentication.spec.ts
@@ -1,17 +1,6 @@
-import { FacebookAuthentication } from '@/domain/features'
 import { AuthenticationError } from '@/domain/errors/'
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
-
-class FacebookAuthenticationUseCase {
-  constructor (
-    private readonly loadFacebookUserApi: LoadFacebookUserApi
-  ) {}
-
-  async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
-    await this.loadFacebookUserApi.loadUser(params)
-    return new AuthenticationError()
-  }
-}
+import { FacebookAuthenticationUseCase } from '@/data/usecases'
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string
