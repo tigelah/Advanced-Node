9881f6e4d607d876f605bfc040942e7342612bc5
tigelah <lokoportitimao2009@hotmail.com>
2021-09-11 17:40:52 -0300

    ensure AxiosHttpClient rethrows if axios throws

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -38,10 +38,19 @@ describe('AxiosHttpClient', () => {
       expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
     })
+
     it('should return data on success', async () => {
       const result = await sut.get({ url, params })
 
       expect(result).toEqual('any_data')
+    })
+
+    it('should rethrow if get throws', async () => {
+      fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+
+      const promise = sut.get({ url, params })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
     })
   })
 })
