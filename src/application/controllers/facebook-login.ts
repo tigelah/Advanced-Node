cd15c994d8b890c53c5d9e0a84c1fc7467a0a9b3
tigelah <lokoportitimao2009@hotmail.com>
2021-09-18 20:37:44 -0300

    refactor: extract common methods into a template

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,8 +1,9 @@
 import { AccessToken } from '@/domain/models'
 import { FacebookAuthentication } from '@/domain/features/facebook-authentication'
-import { HttpResponse, badRequest, unauthorized, serverError, ok } from '@/application/helpers'
-import { ValidationComposite } from '@/application/validation'
-import { ValidationBuilder } from '../validation/builder'
+import { Controller } from '@/application/controllers'
+import { HttpResponse, unauthorized, ok } from '@/application/helpers'
+import { Validator } from '@/application/validation'
+import { ValidationBuilder as Builder } from '../validation/builder'
 
 type HttpRequest = {
   token: string
@@ -11,33 +12,24 @@ type HttpRequest = {
 type Model = Error | {
   accessToken: string
 }
-export class FacebookLoginController {
-  constructor (private readonly facebookAuthentication: FacebookAuthentication) {}
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
-    try {
-      const error = this.validate(httpRequest)
-      if (error !== undefined) {
-        return badRequest(error)
-      }
-      const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
-      if (accessToken instanceof AccessToken) {
-        return ok({
-          accessToken: accessToken.value
-        })
-      } else {
-        return unauthorized()
-      }
-    } catch (error: any) {
-      return serverError(error)
-    }
+export class FacebookLoginController extends Controller {
+  constructor (private readonly facebookAuthentication: FacebookAuthentication) {
+    super()
   }
 
-  private validate (httpRequest: HttpRequest): Error | undefined {
-    return new ValidationComposite([
-      ...ValidationBuilder
+  async perform (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
+    const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
+    return accessToken instanceof AccessToken
+      ? ok({ accessToken: accessToken.value })
+      : unauthorized()
+  }
+
+  override buildValidators (httpRequest: HttpRequest): Validator[] {
+    return [
+      ...Builder
         .of({ value: httpRequest.token, fieldName: 'token' })
         .required()
         .build()
-    ]).validate()
+    ]
   }
 }
