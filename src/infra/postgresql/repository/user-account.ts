bb3987f6bd04ff54b80dc88c3a2fe6f74f0288f7
tigelah <lokoportitimao2009@hotmail.com>
2021-09-19 17:21:30 -0300

    feat: add facebook login integration tests

--- src/infra/postgresql/repository/user-account.ts
+++ src/infra/postgresql/repository/user-account.ts
@@ -9,27 +9,27 @@ type SaveParams = SaveFacebookAccountRepository.Params
 type SaveResult = SaveFacebookAccountRepository.Result
 
 export class PostgreSQLAccountRepository implements LoadUserAccountRepository, SaveFacebookAccountRepository {
-  private readonly pgUserRepo = getRepository(PostgreSQLUser)
-
   async load ({ email }: LoadParams): Promise<LoadResult> {
-    const pgUser = await this.pgUserRepo.findOne({ email })
+    const pgUserRepo = getRepository(PostgreSQLUser)
+    const pgUser = await pgUserRepo.findOne({ email })
 
     if (pgUser !== undefined) {
       return {
-        id: pgUser?.id.toString(),
-        name: pgUser?.name ?? undefined
+        id: pgUser.id.toString(),
+        name: pgUser.name ?? undefined
       }
     }
   }
 
   async saveWithFacebook ({ id, name, email, facebookId }: SaveParams): Promise<SaveResult> {
+    const pgUserRepo = getRepository(PostgreSQLUser)
     let resultId: string
     if (id === undefined) {
-      const pgUser = await this.pgUserRepo.save({ email, name, facebookId })
+      const pgUser = await pgUserRepo.save({ email, name, facebookId })
       resultId = pgUser.id.toString()
     } else {
       resultId = id
-      await this.pgUserRepo.update({ id: parseInt(id) }, { name, facebookId })
+      await pgUserRepo.update({ id: parseInt(id) }, { name, facebookId })
     }
     return { id: resultId }
   }
