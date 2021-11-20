24cea6039b972c38ad5e06e1f34e61490af5d978
tigelah <lokoportitimao2009@hotmail.com>
2021-11-20 12:48:30 -0300

    isolate SUT creation

--- tests/main/adapters/express-router.spec.ts
+++ tests/main/adapters/express-router.spec.ts
@@ -12,7 +12,7 @@ describe('ExpressRouter', () => {
   let controller: MockProxy<Controller>
   let sut: RequestHandler
 
-  beforeEach(() => {
+  beforeAll(() => {
     req = getMockReq({ body: { any: 'any' } })
     res = getMockRes().res
     next = getMockRes().next
@@ -21,8 +21,12 @@ describe('ExpressRouter', () => {
       statusCode: 200,
       data: { data: 'any_data' }
     })
+  })
+
+  beforeEach(() => {
     sut = adaptExpressRoute(controller)
   })
+
   it('should call handle with correct request', async () => {
     await sut(req, res, next)
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
