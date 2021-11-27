61dcdefbded5b5cbd64a439387912ffa7daebfce
tigelah <lokoportitimao2009@hotmail.com>
2021-11-27 12:59:55 -0300

    remove anti pattern middle man

--- src/domain/usecases/authorize.ts
+++
@@ -1,8 +0,0 @@
-import { TokenValidator } from '@/domain/contracts/crypto'
-
-type Setup = (crypto: TokenValidator) => Authorize
-type Input = { token: string }
-type Output = string
-export type Authorize = (input: Input) => Promise<Output>
-
-export const setupAuthorize: Setup = (crypto) => async input => await crypto.validateToken(input)
