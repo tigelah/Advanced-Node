472abce196ab4256233725eb803d033d4c1895d2
tigelah <lokoportitimao2009@hotmail.com>
2022-01-08 10:10:31 -0300

    feat: PgUserProfileRepository returns undefined if id does not exists

--- src/infra/repository/postgresql/user-profile.ts
+++ src/infra/repository/postgresql/user-profile.ts
@@ -3,7 +3,7 @@ import { LoadUserProfile, SaveUserPicture } from '@/domain/contracts/repository'
 
 import { getRepository } from 'typeorm'
 
-export class PgUserProfileRepository implements SaveUserPicture {
+export class PgUserProfileRepository implements SaveUserPicture, LoadUserProfile {
   async savePicture ({ id, pictureUrl, initials }: SaveUserPicture.Input): Promise<void> {
     const pgUserRepo = getRepository(PostgreSQLUser)
     await pgUserRepo.update({ id: parseInt(id) }, { pictureUrl, initials })
