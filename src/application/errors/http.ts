76ac32761244520287ae501646baab9d0a306bbd
tigelah <lokoportitimao2009@hotmail.com>
2022-01-14 18:43:32 -0300

    feat: move code from tests to src

--- src/application/errors/http.ts
+++ src/application/errors/http.ts
@@ -6,13 +6,6 @@ export class ServerError extends Error {
   }
 }
 
-export class RequiredFieldError extends Error {
-  constructor (fieldName: string) {
-    super(`The Field ${fieldName} is required`)
-    this.name = 'RequiredFieldError'
-  }
-}
-
 export class UnauthorizedError extends Error {
   constructor () {
     super('UnauthorizedError')
