8e0390889ad981ce1493c26b84a9b836e371bbc6
tigelah <lokoportitimao2009@hotmail.com>
2022-01-15 16:48:42 -0300

    ensure PUT /users/picture returns 403 is no authorization header is present

--- tests/main/routes/user.spec.ts
+++ tests/main/routes/user.spec.ts
@@ -9,40 +9,46 @@ import { sign } from 'jsonwebtoken'
 import request from 'supertest'
 
 describe('User Routes', () => {
+  let backup: IBackup
+  let pgUserRepo: Repository<PostgreSQLUser>
+
+  beforeAll(async () => {
+    const db = await makeFakeDb([PostgreSQLUser])
+    backup = db.backup()
+    pgUserRepo = getRepository(PostgreSQLUser)
+  })
+
+  afterAll(async () => {
+    await getConnection().close()
+  })
+
+  beforeEach(() => {
+    backup.restore()
+  })
+
   describe('DELETE /users/picture', () => {
-    let backup: IBackup
-    let pgUserRepo: Repository<PostgreSQLUser>
-
-    beforeAll(async () => {
-      const db = await makeFakeDb([PostgreSQLUser])
-      backup = db.backup()
-      pgUserRepo = getRepository(PostgreSQLUser)
-    })
-
-    afterAll(async () => {
-      await getConnection().close()
-    })
-
-    beforeEach(() => {
-      backup.restore()
-    })
-
     it('should return 403 if authorization header is not present', async () => {
       const { status } = await request(app)
         .delete('/api/users/picture')
-
       expect(status).toBe(403)
     })
     it('should return 200 with valid data', async () => {
       const { id } = await pgUserRepo.save({ email: 'any_email', name: 'any name' })
       const authorization = sign({ key: id }, env.jwtSecret)
-
       const { status, body } = await request(app)
         .delete('/api/users/picture')
         .set({ authorization })
-
       expect(status).toBe(200)
       expect(body).toEqual({ pictureUrl: undefined, initials: 'AN' })
+    })
+  })
+
+  describe('PUT /users/picture', () => {
+    it('should return 403 if authorization header is not present', async () => {
+      const { status } = await request(app)
+        .put('/api/users/picture')
+
+      expect(status).toBe(403)
     })
   })
 })
