20ca2cfadbac273687c9089d6f42dfd2c6c847fe
tigelah <lokoportitimao2009@hotmail.com>
2022-01-15 10:51:55 -0300

    add Validators to builder

--- src/application/controllers/save-picture.ts
+++ src/application/controllers/save-picture.ts
@@ -1,5 +1,5 @@
 import { HttpResponse, ok } from '@/application/helpers'
-import { AllowedMimeTypes, MaxFileSize, Required, RequiredBuffer, Validator } from '@/application/validation'
+import { ValidationBuilder as Builder, Validator } from '@/application/validation'
 import { ChangeProfilePicture } from '@/domain/usecases'
 import { Controller } from '@/application/controllers/controller'
 
@@ -18,10 +18,10 @@ export class SavePictureController extends Controller {
 
   override buildValidators ({ file }: HttpRequest): Validator[] {
     return [
-      new Required(file, 'file'),
-      new RequiredBuffer(file.buffer, 'file'),
-      new AllowedMimeTypes(['png', 'jpg'], file.mimeType),
-      new MaxFileSize(5, file.buffer)
+      ...Builder.of({ value: file, fieldName: 'file' })
+        .required()
+        .image({ allowed: ['png', 'jpg'], maxSizeInMb: 5 })
+        .build()
     ]
   }
 }
